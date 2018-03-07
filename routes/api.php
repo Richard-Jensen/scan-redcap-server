bf77a0e71613031a84f03c9c425a57c3f536be72
rbns <kobens@gmail.com>
2018-03-07 13:04:49 +0100

    Get user info with basic auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth.basic')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:api')->get('/token', function (Request $request) {
     return $request->user();
 });
