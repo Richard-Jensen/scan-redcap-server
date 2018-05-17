dddb41c998b4b83d8fd82ef57cec2c3c0e4b9e36
rbns <kobens@gmail.com>
2018-05-17 12:02:50 +0200

    add heroku procfile

--- resources/assets/js/items.js
+++ resources/assets/js/items.js
@@ -1,7 +1,11 @@
-import { items } from './items/2.1/scan.2.1.items.da.json';
+import { items as da } from './items/2.1/scan.2.1.items.da.json';
+import { items as en } from './items/2.1/scan.2.1.items.en.json';
+
+const items = en;
 
 export const getItemByKey = key => items.find(item => item.key === key);
 export const getItemByIndex = index => items[index];
+
 export const getNextItemByKey = key => {
   let index = items.indexOf(getItemByKey(key)) + 1;
   if (!items[index]) index = 0;
