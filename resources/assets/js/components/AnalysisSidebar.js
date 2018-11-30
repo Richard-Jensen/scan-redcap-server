e4720f015adc11b2431c886af5c48fd647fd446f
Richard Jensen <kaffeergaat@gmail.com>
2018-11-30 13:06:44 +0100

    Implemented basic click functionality for the diagnoses on the right sidebar list. Still need to show things like operators (and, or,...) and need to figure out how to be able to dig deeper. I believe adding the required diagnoses as you go along, and recuresively calling the show method is the way forward

--- resources/assets/js/components/AnalysisSidebar.js
+++ resources/assets/js/components/AnalysisSidebar.js
@@ -61,41 +61,88 @@ class AnalysisModal extends Component {
     }
   };
 
-  generateMatchedAlgorithms = (matched, index) => {
-    let className, source, tempSource, sourceKeys;
+  // Old method for generating algorithms
+  // generateMatchedAlgorithms = (matched, index) => {
+  //   let className, source, tempSource, sourceKeys;
+  //   if (matched === 'matched') {
+  //     tempSource = this.state['matchedPrio' + index];
+  //     sourceKeys = Object.keys(tempSource);
+  //     source = sourceKeys.map(key => {
+  //       const diagnosis = Object.assign({}, tempSource[key]);
+  //       this.enhanceDiagnosis(diagnosis);
+  //       return diagnosis;
+  //     });
+  //     className = 'interview-algorithms-evaluator-list-matched-prio' + index.toString();
+  //   }
+  //   else if (matched === 'notMatched') {
+  //     tempSource = this.state['notMatchedPrio' + index];
+  //     sourceKeys = Object.keys(tempSource);
+  //     source = sourceKeys.map(key => {
+  //       const diagnosis = Object.assign({}, tempSource[key]);
+  //       this.enhanceDiagnosis(diagnosis);
+  //       return diagnosis;
+  //     });
+  //     className = 'interview-algorithms-evaluator-list-not-matched-prio' + index.toString();
+  //   }
+  //   this.enhanceDiagnosis(source);
+  //   return (
+  //     Object.keys(source).map(key => {
+  //       return (
+  //         <div
+  //           key={key}
+  //           className={className}
+  //           onClick={this.onClick(source = source, matched = matched, index = index, key = key)}
+  //         >
+  //           <b>{source[key].key}</b>
+  //           <div>{source[key].explanation}</div>
+  //           {source[key].showRequirements &&
+  //             <div>Show Requirements</div>
+  //           }
+  //         </div>
+  //       )
+  //     })
+  //   )
+  // }
+
+  generateEnhancedDiagnoses = (diagnoses) => {
+    const diagnosesClone = Object.keys(diagnoses).map(key => {
+      const diagnosisClone = Object.assign({}, diagnoses[key]);
+      this.enhanceDiagnosis(diagnosisClone);
+      return diagnosisClone;
+    });
+    this.enhanceDiagnosis(diagnosesClone)
+    return diagnosesClone;
+  }
+
+  showMatchedDiagnoses = (diagnoses, matched, index) => {
+    let className;
     if (matched === 'matched') {
-      tempSource = this.state['matchedPrio' + index];
-      sourceKeys = Object.keys(tempSource);
-      source = sourceKeys.map(key => {
-        const diagnosis = Object.assign({}, tempSource[key]);
-        this.enhanceDiagnosis(diagnosis);
-        return diagnosis;
-      });
       className = 'interview-algorithms-evaluator-list-matched-prio' + index.toString();
     }
-    else if (matched === 'notMatched') {
-      tempSource = this.state['notMatchedPrio' + index];
-      sourceKeys = Object.keys(tempSource);
-      source = sourceKeys.map(key => {
-        const diagnosis = Object.assign({}, tempSource[key]);
-        this.enhanceDiagnosis(diagnosis);
-        return diagnosis;
-      });
+    else {
       className = 'interview-algorithms-evaluator-list-not-matched-prio' + index.toString();
     }
-    this.enhanceDiagnosis(source);
     return (
-      Object.keys(source).map(key => {
+      Object.keys(diagnoses).map(key => {
         return (
           <div
             key={key}
             className={className}
-            onClick={this.onClick(source = source, matched = matched, index = index, key = key)}
+            onClick={this.onClick(matched = matched, index = index, key = key)}
           >
-            <b>{source[key].key}</b>
-            <div>{source[key].explanation}</div>
-            {source[key].showRequirements &&
-              <div>Show Requirements</div>
+            <b>{diagnoses[key].key}</b>
+            <div>{diagnoses[key].explanation}</div>
+            {diagnoses[key].showRequirements &&
+              <div>
+                {
+                  diagnoses[key].requirements.map(req => {
+                    console.log(diagnoses[key].algorithm.operator)
+                    return (
+                      <div key={req.id} className='interview-algorithms-subcriterion'>{req.expression}</div>
+                    )
+                  })
+                }
+              </div>
             }
           </div>
         )
@@ -103,30 +150,21 @@ class AnalysisModal extends Component {
     )
   }
 
-  onClick = (source, matched, index, key) => () => {
-    source.showRequirements = true;
-  }
-
-  enhanceDiagnoses = (diagnoses) => {
-
+  onClick = (matched, index, key) => () => {
+    let newState = this.state[matched + 'Prio' + index];
+    newState[key].showRequirements = !newState[key].showRequirements;
+    this.setState({
+      [this.state[matched + 'Prio' + index]]: newState,
+    })
   }
 
   enhanceDiagnosis = (diagnosis) => {
     diagnosis.showRequirements = false;
-    diagnosis.requirements = {};
+    diagnosis.requirements = diagnosis.algorithm ? this.getRequiredDiagnoses(diagnosis) : {};
   }
-
-  addRequirements = (diagnosis, showRequirements) => {
-    if (showRequirements) {
-      return this.getRequiredDiagnoses(diagnosis);
-    }
-    else {
-      return {};
-    }
-  }
-
-  getRequiredDiagnoses = (algorithms, id) => {
-    return algorithms.algorithms.get(id).algorithm.children;
+  
+  getRequiredDiagnoses = (diagnosis) => {
+    return diagnosis.algorithm.children;
   }
 
   render() {
@@ -176,12 +214,12 @@ class AnalysisModal extends Component {
           onClick={() => {
             this.setState({
               evaluated: algorithms.evaluated,
-              matchedPrio1: algorithms.matchedPrio1,
-              matchedPrio2: algorithms.matchedPrio2,
-              matchedPrio3: algorithms.matchedPrio3,
-              notMatchedPrio1: algorithms.notMatchedPrio1,
-              notMatchedPrio2: algorithms.notMatchedPrio2,
-              notMatchedPrio3: algorithms.notMatchedPrio3,
+              matchedPrio1: this.generateEnhancedDiagnoses(algorithms.matchedPrio1),
+              matchedPrio2: this.generateEnhancedDiagnoses(algorithms.matchedPrio2),
+              matchedPrio3: this.generateEnhancedDiagnoses(algorithms.matchedPrio3),
+              notMatchedPrio1: this.generateEnhancedDiagnoses(algorithms.notMatchedPrio1),
+              notMatchedPrio2: this.generateEnhancedDiagnoses(algorithms.notMatchedPrio1),
+              notMatchedPrio3: this.generateEnhancedDiagnoses(algorithms.notMatchedPrio1),
             });
           }}
           className="button"
@@ -199,20 +237,22 @@ class AnalysisModal extends Component {
             </option>
           ))}
         </select>
-        <div className="interview-algorithms-evaluator-list">
-          <h4>Matched Algorithms (first priority)</h4>
-          {this.generateMatchedAlgorithms('matched', 1)}
-          <h4>Matched Algorithms (second priority)</h4>
-          {this.generateMatchedAlgorithms('matched', 2)}
-          <h4>Matched Algorithms (third priority)</h4>
-          {this.generateMatchedAlgorithms('matched', 3)}
-          <h4>Not Matched Algorithms (first priority)</h4>
-          {this.generateMatchedAlgorithms('notMatched', 1)}
-          <h4>Not Matched Algorithms (second priority)</h4>
-          {this.generateMatchedAlgorithms('notMatched', 2)}
-          <h4>Not Matched Algorithms (third priority)</h4>
-          {this.generateMatchedAlgorithms('notMatched', 3)}
-        </div>
+        {algorithms &&
+          <div className="interview-algorithms-evaluator-list">
+            <h4>Matched Algorithms (first priority)</h4>
+            {this.showMatchedDiagnoses(this.state.matchedPrio1, 'matched', 1)}
+            <h4>Matched Algorithms (second priority)</h4>
+            {this.showMatchedDiagnoses(this.state.matchedPrio2, 'matched', 2)}
+            <h4>Matched Algorithms (third priority)</h4>
+            {this.showMatchedDiagnoses(this.state.matchedPrio3, 'matched', 3)}
+            <h4>Not Matched Algorithms (first priority)</h4>
+            {this.showMatchedDiagnoses(this.state.notMatchedPrio1, 'notMatched', 1)}
+            <h4>Not Matched Algorithms (second priority)</h4>
+            {this.showMatchedDiagnoses(this.state.notMatchedPrio2, 'notMatched', 2)}
+            <h4>Not Matched Algorithms (third priority)</h4>
+            {this.showMatchedDiagnoses(this.state.notMatchedPrio3, 'notMatched', 3)}
+          </div>
+        }
       </div>
     );
   }
